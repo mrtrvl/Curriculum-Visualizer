3033ab97c1bd273c82e71adc8a3f79c8f79899a4
mrtrvl <mrt@mrt.ee>
2023-07-02 15:38:54 +0300

    Structuring

---
+++ public/js/relations.js
@@ -0,0 +1,81 @@
+const relations = [
+  {
+    data: {
+      id: 1,
+      source: 'Programmeerimise alused',
+      target: 'Andmebaasid'
+    }
+  },
+  {
+    data: {
+      id: 2,
+      source: 'Programmeerimise alused',
+      target: 'Programmeerimine I'
+    }
+  },
+  {
+    data: {
+      id: 3,
+      source: 'Programmeerimise alused',
+      target: 'Veebirakendused ja nende loomine'
+    }
+  },
+  {
+    data: {
+      id: 4,
+      source: 'Andmebaasid',
+      target: 'Andmebaasid II'
+    }
+  },
+  {
+    data: {
+      id: 5,
+      source: 'Veebiprogrammeerimine',
+      target: 'Veebiraamistikud'
+    }
+  },
+  {
+    data: {
+      id: 6,
+      source: 'Programmeerimine I',
+      target: 'Programmeerimine II'
+    }
+  },
+  {
+    data: {
+      id: 7,
+      source: 'Veebirakendused ja nende loomine',
+      target: 'Veebiraamistikud'
+    }
+  },
+  {
+    data: {
+      id: 9,
+      source: 'Andmebaasid',
+      target: 'Programmeerimine II'
+    }
+  },
+  {
+    data: {
+      id: 10,
+      source: 'Programmeerimine I',
+      target: 'Programmeerimine III'
+    }
+  },
+/*       {
+    data: {
+      id: 11,
+      source: 'Programmeerimine II',
+      target: 'Andmebaasid II',
+      cooperation: 'true',
+    }
+  }, */
+/*       {
+    data: {
+      id: 12,
+      source: 'Programmeerimine II',
+      target: 'Veebiraamistikud',
+      cooperation: 'true',
+    }
+  }, */
+];
---
+++ public/js/subjects.js
@@ -0,0 +1,128 @@
+const subjects = [
+  {
+    data: {
+      id: 'Programmeerimise alused',
+      volume: 6,
+      mandatory: 'true',
+      semester: 1,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Multimeedium',
+      volume: 4,
+      mandatory: 'true',
+      semester: 1,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Andmebaasid',
+      volume: 3,
+      mandatory: 'true',
+      semester: 2,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Veebirakendused ja nende loomine',
+      volume: 3,
+      mandatory: 'true',
+      semester: 2,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Sissejuhatus tarkvaraarendusse',
+      volume: 4,
+      mandatory: 'true',
+      semester: 2,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Veebiraamistikud',
+      volume: 4,
+      mandatory: 'true',
+      semester: 3,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Veebiprogrammeerimine',
+      volume: 5,
+      mandatory: 'true',
+      semester: 1,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Andmebaasid II',
+      volume: 3,
+      mandatory: 'false',
+      semester: 3,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Programmeerimine I',
+      volume: 5,
+      mandatory: 'false',
+      semester: 2,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Programmeerimine II',
+      volume: 5,
+      mandatory: 'false',
+      semester: 3,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Programmeerimine III',
+      volume: 5,
+      mandatory: 'false',
+      semester: 4,
+      category: 'Eriala ained',
+    }
+  },
+  {
+    data: {
+      id: 'Õppimine kõrgkoolis',
+      volume: 6,
+      mandatory: 'true',
+      semester: 1,
+      category: 'Üleülikoolilised ained',
+    }
+  },
+  {
+    data: {
+      id: 'Üld- ja sotsiaalpsühholoogia',
+      volume: 6,
+      mandatory: 'true',
+      semester: 4,
+      category: 'Üleülikoolilised ained',
+    }
+  },
+  {
+    data: {
+      id: 'ELU - Erialasid Lõimiv Uuendus',
+      volume: 6,
+      mandatory: 'true',
+      semester: null,
+      category: 'Üleülikoolilised ained',
+    }
+  },
+];
