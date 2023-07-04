c73dfb4fe3573d147ac9cdc0049753f135d5fb27
mrtrvl <mrt@mrt.ee>
2023-07-04 12:46:45 +0300

    Add collapsible table to subjects bage

--- frontend/src/pages/SubjectsPage.js
+++ frontend/src/pages/SubjectsPage.js
@@ -1,12 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../config';
 
-function SubjectsPage() {
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+const SubjectsPage = () => {
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${config.apiUrl}/curriculums`);
+      console.log(response.data[1]);
+      setSubjects(response.data[1].subjects);
+    };
+
+    fetchData();
+  }, []);
+
+  function SubjectRow(props) {
+    const { subject } = props;
+    const [open, setOpen] = React.useState(false);
+
+    return (
+      <React.Fragment>
+        <TableRow key={subject.data.id}>
+          <TableCell>
+            <IconButton size="small" onClick={() => setOpen(!open)}>
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell component="th" scope="row">
+            {subject.data.id}
+          </TableCell>
+          <TableCell>{subject.data.volume}</TableCell>
+          <TableCell>{subject.data.category}</TableCell>
+          <TableCell>{subject.data.parent}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box margin={1}>
+                {/* This is where you'll add the details for your subject */}
+                <Typography variant="h6" gutterBottom component="div">
+                  Details
+                </Typography>
+                {/* Insert your detailed information about subject here. */}
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+
   return (
-    <div>
-      <h1>Subjects Page</h1>
-      {/* Subjects List Goes Here */}
-    </div>
+    <>
+      <h1>Subjects</h1>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nimi</TableCell>
+              <TableCell>EAP</TableCell>
+              <TableCell>Moodul</TableCell>
+              <TableCell>Semester</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {subjects.map((subject) => (
+              <SubjectRow key={subject.data.id} subject={subject} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
-}
+};
 
 export default SubjectsPage;
